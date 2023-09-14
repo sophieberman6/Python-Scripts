80739922b4636ce99fcdb018bf7615cc35ef5249
sophieberman6 <145056893+sophieberman6@users.noreply.github.com>
2023-09-14 13:32:00 -0500

    Add files via upload

---
+++ 6digit.py
@@ -0,0 +1,39 @@
+from itertools import product
+
+# Function to generate all possible 4-digit numbers based on user input
+def generate_all_possible_numbers():
+    user_input = input("Enter the digits (0-9) you want to include (separated by spaces): ")
+    
+    # Split the user input into a list of digits
+    selected_digits = [int(digit) for digit in user_input.split()]
+    # selected_digits.append(0)
+    
+    # Check if the user has selected at least 1 digit
+    if len(selected_digits) == 0:
+        print("No digits selected. Please choose at least one digit (0-9).")
+        return
+    
+    # Generate all possible 4-digit numbers based on selected digits
+    
+    output_file_path = '6digit.txt'
+    
+    with open(output_file_path, 'w') as file:
+            
+
+        for combination in product(selected_digits, repeat=6):
+            number = int(''.join(map(str, combination)))
+            digit = (f"{number:06d}")
+            print(digit)
+            file.write(f"{digit}\n")
+
+
+    
+    # Write the generated numbers to the output file
+
+    
+    print(f"All generated numbers saved to '{output_file_path}'.")
+
+
+
+# Call the function to generate all possible numbers
+generate_all_possible_numbers()
